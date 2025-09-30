9941f7a4d22a435b6333964d777f2da530d0a53a
chamonsheikh121 <studentchamon1212@gmail.com>
2025-10-01 02:42:21 +0600

    generating access token by  refresh token

--- src/redux/api/base_api.ts
+++ src/redux/api/base_api.ts
@@ -1,5 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { RootState } from "../store";
+import { set_user } from "../features/auth/auth_slice";
 
 const base_query = fetchBaseQuery({
   baseUrl: "http://localhost:1000/api/v1",
@@ -12,8 +13,39 @@ const base_query = fetchBaseQuery({
   },
 });
 
+const base_query_with_refresh_token = async (args, api, extraOptions) => {
+  let result = await base_query(args, api, extraOptions);
+  if (
+    result?.error?.status == 401 &&
+    result?.error?.data?.message == "jwt expired"
+  ) {
+    console.log("sending refresh token");
+    const res = await fetch("http://localhost:1000/api/v1/auth/refresh-token", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    const user = (api.getState() as RootState).auth.user;
+
+    console.log({
+      user,
+      token: data.data.accessToken,
+    });
+    api.dispatch(
+      set_user({
+        user,
+        token: data.data.accessToken,
+      })
+    );
+
+    result = await base_query(args, api, extraOptions);
+  }
+  return result;
+};
+
 export const base_api = createApi({
   reducerPath: "base_api",
-  baseQuery: base_query,
+  baseQuery: base_query_with_refresh_token,
   endpoints: () => ({}),
 });
